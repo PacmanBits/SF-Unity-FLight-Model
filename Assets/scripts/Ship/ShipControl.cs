99ed9a6f8cc0b5c572e6870fbf44d70b48db8fcc
Sandy Gifford <sanford.r.gifford@gmail.com>
2015-12-07 20:55:00 -0500

    Added basic support for visual roll during yaw turns

--- Assets/scripts/Ship/ShipControl.cs
+++ Assets/scripts/Ship/ShipControl.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class ShipControl : ShipComponent {
 
-
+	public float maxRoll = 20;
+	public readonly float MIN_HEADING = -1;
+	public readonly float MAX_HEADING = 1;
 	
 	// Use this for initialization
 	void Start () {
@@ -12,7 +14,12 @@ public class ShipControl : ShipComponent {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector2 heading = ship.pilot.getHeading ();
+		Vector2 heading = clampVector(ship.pilot.getHeading (), MIN_HEADING, MAX_HEADING);
 		ship.rb.angularVelocity = new Vector3(0, heading.x, heading.y);
+		ship.rollObj.rotation = Quaternion.AngleAxis(-1 * heading.x * maxRoll, transform.forward);
+	}
+
+	private Vector2 clampVector(Vector2 vec, float min, float max) {
+		return new Vector2 (Mathf.Clamp (vec.x, min, max), Mathf.Clamp (vec.y, min, max));
 	}
 }
