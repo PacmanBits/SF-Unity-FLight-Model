3a8969646e34b8168b0699a6cd9a00e59032a066
Sandy Gifford <sanford.r.gifford@gmail.com>
2015-12-18 00:35:58 -0500

    Basic Explosion and Health

---
+++ Assets/scripts/Other/Explosion.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+	ParticleSystem part;
+
+	// Use this for initialization
+	void Start () {
+		part = GetComponent<ParticleSystem> ();
+
+		enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (part.IsAlive (true))
+			Destroy (gameObject);
+	}
+
+	public void Explode() {
+		if (!enabled) {
+			enabled = true;
+			part.Play();
+		}
+	}
+}
