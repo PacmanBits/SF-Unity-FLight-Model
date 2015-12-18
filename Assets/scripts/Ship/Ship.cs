c12013f1bc7bf7736191a333fcedda670da6d159
Sandy Gifford <sanford.r.gifford@gmail.com>
2015-12-18 00:36:55 -0500

    Simplified structure

--- Assets/scripts/Ship/Ship.cs
+++ Assets/scripts/Ship/Ship.cs
@@ -3,38 +3,37 @@ using System.Collections;
 
 public class Ship : MonoBehaviour {
 	public Rigidbody   rb      { get; private set; }
+	public ShipHealth  health  { get; private set; }
+	public ShipPilot   pilot   { get; private set; }
 	public ShipControl control { get; private set; }
 	public ShipEngine  engine  { get; private set; }
-	public ShipPilot   pilot   { get; private set; }
 	public ShipCamera  cam     { get; private set; }
 
 
 
 	void Awake () {
-		rb      = gameObject.GetComponent<Rigidbody>   ();
-		pilot   = gameObject.GetComponent<ShipPilot>   ();
-		engine  = gameObject.GetComponent<ShipEngine>  ();
-		control = gameObject.GetComponent<ShipControl> ();
-		cam     = gameObject.GetComponent<ShipCamera>  ();
-		
-		if (rb == null) {
-			Debug.LogWarning("No Rigidbody component found on ship, creating one.");
-			rb = gameObject.AddComponent<Rigidbody>();
-		}
-		
-		if (pilot == null) {
-			Debug.LogWarning("No pilot component found on ship, creating dumb pilot.");
-			pilot = gameObject.AddComponent<ShipPilot>();
-		}
-		
-		if (engine == null)
-			Debug.LogWarning ("No engine found on ship.");
-		
-		if (control == null)
-			Debug.LogWarning ("No control found on ship.");
-		
-		if (cam == null)
-			Debug.LogWarning ("No camera controller found on ship.");
+		rb      = checkForComponent<Rigidbody>(true)  ;
+		pilot   = checkForComponent<ShipPilot>(true)  ;
+		health  = checkForComponent<ShipHealth>(true) ;
+		engine  = checkForComponent<ShipEngine>()     ;
+		control = checkForComponent<ShipControl>()    ;
+		cam     = checkForComponent<ShipCamera>()     ;
+	}
 
+	private T checkForComponent<T>(bool addIfNotFound = false) where T: Component {
+		T comp = gameObject.GetComponent<T> ();
+
+		if (comp == null) {
+			string name = "fakeType";
+
+			if(addIfNotFound) {
+				Debug.LogWarning("No " + name + " component found on ship, creating basic " + name + ".");
+				comp = gameObject.AddComponent<T>();
+			} else {
+				Debug.LogWarning ("No " + name + " component found on ship.");
+			}
+		}
+
+		return comp;
 	}
 }
