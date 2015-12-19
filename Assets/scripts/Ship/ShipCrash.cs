1b39a6ebb12d0c21a6ea0d33641c0e56b77e13bd
Sandy Gifford <sanford.r.gifford@gmail.com>
2015-12-19 14:11:44 -0500

    Crashes Hurt

--- Assets/scripts/Ship/ShipCrash.cs
+++ Assets/scripts/Ship/ShipCrash.cs
@@ -2,13 +2,45 @@
 using System.Collections;
 
 public class ShipCrash : ShipComponent {
-	private int hits = 0;
+	public float damageFactor = 1.0f;
+	
 	void OnCollisionEnter(Collision collision) {
+
+		//Debug.Log (collision.impulse.magnitude);
+		/*
+		Ray aveNorm = averageNormal (collision.contacts);
+		Debug.DrawRay (aveNorm.origin, aveNorm.direction, Color.green, 1f);
+
+		float angle = Vector3.Angle (transform.forward, aveNorm.direction);
+
+		Debug.Log (angle);
+
+		Rigidbody otherRB = collision.rigidbody;
+		*/
+
+		float damage = collision.impulse.magnitude * damageFactor;
+
+		ship.health.damage (damage);
+
+		/*
 		ContactPoint point = collision.contacts [0];
 
 		Debug.DrawRay (point.point, point.normal * 10, Color.white, 1);
 		ship.rb.AddForce (point.normal * 100);
 		//transform.forward = point.normal;
 		GetComponent<Shake> ().shake ();
+		*/
+	}
+
+	private Ray averageNormal(ContactPoint[] contacts) {
+		Vector3 averageNormal = Vector3.zero;
+		Vector3 averagePoint  = Vector2.zero;
+
+		foreach (var contact in contacts) {
+			averageNormal += contact.normal ;
+			averagePoint  += contact.point  ;
+		}
+
+		return new Ray(averagePoint / contacts.Length, averageNormal / contacts.Length);
 	}
 }
--- Assets/scripts/Ship/ShipHealth.cs
+++ Assets/scripts/Ship/ShipHealth.cs
@@ -2,21 +2,48 @@
 using System.Collections;
 
 public class ShipHealth : MonoBehaviour {
+	
+	public float maxHealth = 100;
 
 	public float health { get; private set; }
-	public float maxHealth { get; private set; }
 	public float healthFrac {
 		get {
 			return health / maxHealth;
-		};
+		}
+	}
+
+	public GameObject explosion;
+
+	void Start() {
+		health = maxHealth;
 	}
 
 	public float damage(float amount) {
-		return heal (-1 * amount);
+		Debug.Log ("Damaging for " + amount);
+		
+		return setHealth (health - amount);
 	}
 
 	public float heal(float amount) {
-		health += amount;
-		return health;
+		Debug.Log ("Healing for " + amount);
+
+		return setHealth (health + amount);
+	}
+
+	public float setHealth(float amount) {
+
+		Debug.Log ("Setting health to " + amount);
+
+		health = Mathf.Clamp (amount, 0, maxHealth);
+
+		if (health == 0)
+			kill ();
+
+		return 0;
+	}
+
+	public void kill() {
+		Instantiate (explosion, transform.position, Quaternion.identity);
+		Destroy (gameObject);
 	}
 }
