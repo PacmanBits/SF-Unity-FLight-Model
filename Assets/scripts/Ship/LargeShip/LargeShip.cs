6d339955cb6b4f8eec766d5dbdcd2aa62fca525a
Sandy Gifford <sanford.r.gifford@gmail.com>
2016-01-21 18:16:28 -0500

    Basic Large Ship Stuff

---
+++ Assets/scripts/Ship/LargeShip/LargeShip.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LargeShip : ExMonoBehavior {
+
+	    ////////////////////////
+	   ////                ////
+	  ////   Properties   ////
+	 ////                ////
+	////////////////////////
+
+	  ////////////////////////
+	 //  public            //
+	////////////////////////
+
+	public Rigidbody   rb      { get; private set; }
+	public ShipHealth  health  { get; private set; }
+
+	public SmallShipPilot   pilot   { get; private set; }
+	public SmallShipControl control { get; private set; }
+	public SmallShipEngine  engine  { get; private set; }
+	public SmallShipCamera  cam     { get; private set; }
+
+	  ////////////////////////
+	 //  protected         //
+	////////////////////////
+
+	  ////////////////////////
+	 //  private           //
+	////////////////////////
+
+	    ////////////////////////
+	   ////                ////
+	  ////     Unity      ////
+	 ////                ////
+	////////////////////////
+
+	protected virtual void Awake () {
+		rb      = checkForComponent<Rigidbody>(true)  ;
+		health  = checkForComponent<ShipHealth>(true) ;
+		
+		pilot   = checkForComponent<SmallShipPilot>(true)  ;
+		control = checkForComponent<SmallShipControl>()    ;
+		engine  = checkForComponent<SmallShipEngine>()     ;
+		cam     = checkForComponent<SmallShipCamera>()     ;
+	}
+	
+	    ////////////////////////
+	   ////                ////
+	  ////    Methods     ////
+	 ////                ////
+	////////////////////////
+
+	  ////////////////////////
+	 //  public            //
+	////////////////////////
+
+	  ////////////////////////
+	 //  protected         //
+	////////////////////////
+
+	  ////////////////////////
+	 //  private           //
+	////////////////////////
+}
