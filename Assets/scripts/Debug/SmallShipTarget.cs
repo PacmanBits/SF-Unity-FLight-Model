0586742546f3a82508dc69445b5f16d716371d92
Sandy Gifford <sanford.r.gifford@gmail.com>
2016-01-22 18:37:05 -0500

    Target script that moves when ship gets close to it

---
+++ Assets/scripts/Debug/SmallShipTarget.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SmallShipTarget : MonoBehaviour {
+
+	    ////////////////////////
+	   ////                ////
+	  ////   Properties   ////
+	 ////                ////
+	////////////////////////
+
+	  ////////////////////////
+	 //  public            //
+	////////////////////////
+
+	public Vector3 fieldSize = new Vector3(50, 50, 50);
+
+	  ////////////////////////
+	 //  protected         //
+	////////////////////////
+
+	  ////////////////////////
+	 //  private           //
+	////////////////////////
+
+	    ////////////////////////
+	   ////                ////
+	  ////     Unity      ////
+	 ////                ////
+	////////////////////////
+
+	void OnTriggerEnter(Collider other) {
+		if (other.GetComponent<SmallShip> () != null)
+			this.transform.position = newRandomPosition();
+	}
+
+	    ////////////////////////
+	   ////                ////
+	  ////    Methods     ////
+	 ////                ////
+	////////////////////////
+
+	  ////////////////////////
+	 //  public            //
+	////////////////////////
+
+	  ////////////////////////
+	 //  protected         //
+	////////////////////////
+
+	  ////////////////////////
+	 //  private           //
+	////////////////////////
+
+	private Vector3 newRandomPosition() {
+		return new Vector3(
+			Random.Range(fieldSize.x / -2, fieldSize.x / 2),
+			Random.Range(fieldSize.y / -2, fieldSize.y / 2),
+			Random.Range(fieldSize.z / -2, fieldSize.z / 2)
+		);
+	}
+}
